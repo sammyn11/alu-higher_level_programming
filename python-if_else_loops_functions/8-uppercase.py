7410e46c47d5b6d6103c73393894b170b3523fa9
sammyn11 <s.niyonkuru@alustudent.com>
2024-06-28 00:23:59 -0700

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print(uppercase_char.format(), end='')
+    print()
