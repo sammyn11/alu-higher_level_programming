5fd201e56210d226ec724e4a8fded1723849bec6
sammyn11 <s.niyonkuru@alustudent.com>
2024-08-05 19:40:40 +0000

    3-methods.sh

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,2 +1,3 @@
-end an OPTIONS request using curl to display the allowed HTTP methods
+#!/bin/bash
+# Send an OPTIONS request using curl to display the allowed HTTP methods
 curl -sI -X OPTIONS "$1" | grep -i allow | cut -d ' ' -f 2-
